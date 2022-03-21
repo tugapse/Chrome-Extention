d678e790885c6bf5b8be2b75895be8ad8a712430
Fabio Almeida <fabio.almeida@lovys.com>
2022-03-21 21:14:51 +0000

    add block fold coloring

--- JsonFolding/tree-node.js
+++ JsonFolding/tree-node.js
@@ -13,13 +13,9 @@ class TreeNode {
   }
 
   collapse() {
-    const lines = this.jsonEditor.editorAddon.lines;
-    const linesArray = Array.prototype.slice.call(lines);
-    const startIndex = linesArray.indexOf(this.htmlElemnt) + 1;
-    const endIndex = linesArray.indexOf(this.connectedNode.htmlElemnt);
-    const validLines = linesArray.slice(startIndex, endIndex);
-    this.htmlElemnt.classList.add("collapsed");
+    const validLines = this._getValidLines();
     validLines.forEach((line) => this._addLine(line));
+    this.htmlElemnt.classList.add("collapsed");
     this.foldElement.textContent = "+";
     this.collapsed = true;
     this.htmlElemnt.foldNode = this;
@@ -37,17 +33,51 @@ class TreeNode {
 
   initStartFold(editor) {
     this.jsonEditor = editor;
-    this.htmlElemnt.onclick = (e) => this._checkExpand(e);
+    this.addEvents();
     this.htmlElemnt.classList.add("has-fold");
     this._createFoldElement();
     this.htmlElemnt.insertBefore(this.foldElement, this.htmlElemnt.firstChild);
   }
-
+  addEvents() {
+    this.htmlElemnt.onclick = (e) => this._checkExpand(e);
+    this.htmlElemnt.onmouseenter = (e) => this._highlightNodes();
+    this.htmlElemnt.onmouseleave = (e) => this._highlightNodes(false);
+  }
   initEndFold(editor, startNode) {
     this.jsonEditor = editor;
     this.connectedNode = startNode;
     this.htmlElemnt.classList.add("fold-end");
     startNode.connectedNode = this;
+  }
+
+  _getValidLines() {
+    const lines = this.jsonEditor.editorAddon.lines;
+    const linesArray = Array.prototype.slice.call(lines);
+    const startIndex = linesArray.indexOf(this.htmlElemnt) + 1;
+    const endIndex = linesArray.indexOf(this.connectedNode.htmlElemnt);
+    const validLines = linesArray.slice(startIndex, endIndex);
+    return validLines;
+  }
+  _highlightNodes(highlight = true) {
+    const lineClass = "highlight";
+
+    highlight
+      ? this.htmlElemnt.classList.add(lineClass)
+      : this.htmlElemnt.classList.remove(lineClass);
+
+    this._getValidLines().forEach((line) => {
+      if (highlight) {
+        line.classList.add(lineClass);
+      } else {
+        line.classList.remove(lineClass);
+      }
+    });
+    if (this.connectedNode) {
+      const elm = this.connectedNode.htmlElemnt;
+      highlight
+        ? elm.classList.add(lineClass)
+        : elm.classList.remove(lineClass);
+    }
   }
   _checkExpand(e) {
     this.collapsed && !e.foldDone && this.expand();
